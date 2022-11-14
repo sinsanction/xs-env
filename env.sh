324ac0d4f0e456102332ccb60f80fe5ce09e3632
sinsanction <1602723930@qq.com>
2022-11-14 19:49:21 +0800

    fix setup scripts

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 # This script will setup RV-CVP environment variables
 
+export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
 export NOOP_HOME=$(pwd)/NutShell
--- setup.sh
+++ setup.sh
@@ -9,3 +9,9 @@ git submodule update --init --recursive
 # Setup RV-CVP environment variables
 source env.sh
 # OPTIONAL: export them to .bashrc
+
+cd ${NEMU_HOME}
+make riscv64-xs-ref_defconfig
+make -j
+
+cd ${XS_PROJECT_ROOT}
